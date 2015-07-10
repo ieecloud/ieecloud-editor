5138801c6c22da4ae686db75c9fcf2471f3385c8
Elena_Chekanina <Elena_Chekanina@epam.com>
2015-07-10 22:07:14 +0300

    fixed karma

--- karma.conf.js
+++ karma.conf.js
@@ -5,10 +5,11 @@ module.exports = function(config){
 
     files : [
       'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'app/bower_components/jquery/dist/jquery.min.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/editor*/**/*.js'
     ],
 
     autoWatch : true,
